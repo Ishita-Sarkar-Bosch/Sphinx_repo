867553bad3f2c0f17c3acdd7bea74942efd8c205
Ishita-Sarkar-Bosch <125953368+Ishita-Sarkar-Bosch@users.noreply.github.com>
2023-03-05 00:39:23 +0000

    updating conf.py from https://github.com/speedyleion/sphinx-c-autodoc/blob/main/docs/conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,10 +6,16 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+SOURCE_DIR = os.path.abspath(os.path.join('.', '..', 'src'))
+sys.path.insert(0, SOURCE_DIR)
+
 project = 'SphinxTryout'
 copyright = '2023, Blesson'
 author = 'Blesson'
 release = 'PST'
+
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
@@ -19,10 +25,20 @@ extensions = []
 templates_path = ['_templates']
 exclude_patterns = []
 
-language = 'C'
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+
+# The master toctree document.
+master_doc = 'index'
+
+
+language = 'en'
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
